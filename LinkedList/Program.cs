c2dec03c70bc7026caf5673fa5a78d3f6b923acb
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-04-10 22:59:27 -0300

    LinkedList - Alura Course Collection - Lesson 05

---
+++ LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> frutas = new List<string>
+            {
+                 "abacate", "banana", "caqui", "damasco", "figo"
+            };
+            //vamos imprimir essa lista
+            foreach (var fruta in frutas)
+            {
+                Console.WriteLine(fruta);
+            }
+
+        }
+    }
+}
